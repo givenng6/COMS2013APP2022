febcace5e777fa521603272e1981fa03d57e24b3
givenng6 <givenng6@gmail.com>
2022-05-28 10:33:53 +0200

    Init

--- K-Means_StraightLine/src/Point.java
+++ K-Means_StraightLine/src/Point.java
@@ -1,18 +1,36 @@
 public class Point {
 
-    private int id, x;
-
-    public Point(int id, int x){
+    private int id;
+    private double x;
+    private Centroid currCentroid;
+    public Point(int id, double x){
         this.id = id;
         this.x = x;
+        this.currCentroid = null;
     }
 
     public int getId() {
         return id;
     }
 
-    public int getX() {
+    public double getX() {
         return x;
+    }
+
+    public Centroid getCurrCentroid(){
+        return currCentroid;
+    }
+
+    public double getDistance(){
+        return distance(currCentroid.getX());
+    }
+
+    public double distance(double centroidX){
+        return Math.abs(centroidX - this.x);
+    }
+
+    public void addCentroid(Centroid centroid){
+        this.currCentroid = centroid;
     }
 
 
